ed38f1732bbf5586b3e0c73969a65d93867a1d05
Alexandr <taka.alex99@hotmail.com>
2017-12-22 16:20:46 -0800

    2

--- WebRider/StartForm.cs
+++ WebRider/StartForm.cs
@@ -12,9 +12,14 @@ namespace WebRider
 {
     public partial class StartForm : Form
     {
+        public string host_text = "localhost";
+        public string port_text = "3306";
+        public string db_user_text = "root";
+        public string db_pass_text = "";
+        public string db_name_text = "test";
         public StartForm()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,13 +29,14 @@ namespace WebRider
 
         private void administrator_Click(object sender, EventArgs e)
         {
-            LoginForm lf = new LoginForm();
+            LoginForm lf = new LoginForm(this);
             lf.Show();
+            Console.WriteLine(this.db_name_text);
         }
 
         private void student_login_Click(object sender, EventArgs e)
         {
-            Attendance attendance_form = new Attendance();
+            Attendance attendance_form = new Attendance(this);
             attendance_form.Show();
             attendance_form.WindowState = FormWindowState.Maximized;
         }
