d37e18e47dbbd28b0a79e7399c82fade2789cebc
Alexandr <taka.alex99@hotmail.com>
2017-12-22 16:04:26 -0800

    Upload Registration

---
+++ WebRider/Registration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebRider
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+            this.fingerPrintRegUserControl.RegistrationCompletedStatusChanged += new StatusChangedEventHandler(fingerPrintRegUserControl_RegistrationCompleted);
+        }
+
+        private void fingerPrintRegUserControl_RegistrationCompleted(object sender, StatusChangedEventArgs e)
+        {
+
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            if (this.fingerPrintRegUserControl.IsRegistrationComplete == false)
+            {
+                MessageBox.Show("Didn't Capture Fingerprint yet");
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
---
+++ WebRider/Registration.designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WebRider
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fingerPrintRegUserControl = new WebRider.RegistrationUserControl();
+            this.Ok = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fingerPrintRegUserControl
+            // 
+            this.fingerPrintRegUserControl.Location = new System.Drawing.Point(3, 12);
+            this.fingerPrintRegUserControl.Name = "fingerPrintRegUserControl";
+            this.fingerPrintRegUserControl.Size = new System.Drawing.Size(263, 342);
+            this.fingerPrintRegUserControl.TabIndex = 22;
+            // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(15, 382);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 23;
+            this.Ok.Text = "OK";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(109, 382);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 24;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 417);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Ok);
+            this.Controls.Add(this.fingerPrintRegUserControl);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public RegistrationUserControl fingerPrintRegUserControl;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Button Cancel;
+    }
+}
